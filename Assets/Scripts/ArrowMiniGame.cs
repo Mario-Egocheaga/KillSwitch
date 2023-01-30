700f576f820c812c19e9409c90248fbfa9f8260c
Mario Egocheaga <43833659+Mario-Egocheaga@users.noreply.github.com>
2023-01-29 20:13:07 -0500

    Added Virus Removal

--- Assets/Scripts/ArrowMiniGame.cs
+++ Assets/Scripts/ArrowMiniGame.cs
@@ -12,17 +12,17 @@ public class ArrowMiniGame : MonoBehaviour
             transform.Translate(0, 1.25f, 0);
         else if (Input.GetKeyDown(KeyCode.DownArrow) & transform.position.y > -3)
             transform.Translate(0, -1.25f, 0);
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) & transform.position.x > -5.5)
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) & transform.position.x > -5)
             transform.Translate(-1.25f, 0, 0);
-        else if (Input.GetKeyDown(KeyCode.RightArrow) & transform.position.x < 5.5)
+        else if (Input.GetKeyDown(KeyCode.RightArrow) & transform.position.x < 5)
             transform.Translate(1.25f, 0, 0);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Exit")
+        if (collision.gameObject.tag == "Exit")
         {
-            Destroy(gameObject);
+            Destroy(collision.gameObject);
         }
     }
 }
